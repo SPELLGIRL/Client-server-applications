b5b5c3c780bb4d0628da30c8b38b62a628c463eb
SPELLGIRL <chepushilka@yandex.ru>
2019-07-12 15:03:45 +0300

    Reformat client side

--- messenger/client/jim/config.py
+++ messenger/client/jim/config.py
@@ -1,20 +1,34 @@
 """Константы для jim протокола, настройки"""
+# Формат даты
+DATE_FORMAT = '%d-%m-%Y %H:%M'
+# Разделитель сообщений
+DELIMITER = '\0'
+
 # Ключи
 # тип сообщения между клиентом и сервером
 ACTION = 'action'
 # время запроса
 TIME = 'time'
-# данные о пользователе - клиенте (вложенный словарь)
-USER = 'user'
-# имя пользователя - чата
-ACCOUNT_NAME = 'account_name'
+# данные о пользователе - клиенте
+USER = 'user_name'
 # код ответа
 RESPONSE = 'response'
+# кому
+TO = 'to'
+# от кого
+FROM = 'sender'
+# сообщение
+TEXT = 'text'
+# Количество
+QUANTITY = 'quantity'
 # текст ошибки
 ERROR = 'error'
 
 # Значения
 PRESENCE = 'presence'
+SEND_MSG = 'send_msg'
+GET_CONTACTS = 'get_contacts'
+READ_MESSAGES = 'read_messages'
 
 # Коды ответов
 BASIC_NOTICE = 100
